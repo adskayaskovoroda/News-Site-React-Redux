c89b90e3564d7916d44981c51f8d70272fcdb521
Pavel Kotlyarov <p.kotlyarov@dunice.net>
2020-10-21 16:42:26 +0300

    urls for NewsViewSet

--- back/news/news/urls.py
+++ back/news/news/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from news_app import urls as NewsAppUrls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+urlpatterns += NewsAppUrls.urlpatterns
