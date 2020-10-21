b42057a8f3209ff442f67da5b6a34d542e88b789
Pavel Kotlyarov <p.kotlyarov@dunice.net>
2020-10-21 17:23:11 +0300

    NewsSerializer to ModelSerializer

--- back/news/news_app/serializers/news_serializer.py
+++ back/news/news_app/serializers/news_serializer.py
@@ -2,20 +2,7 @@ from rest_framework import serializers
 from ..models import News
 
 
-class NewsSerializer(serializers.Serializer):
-    
-    ID = serializers.IntegerField(read_only=True, required=False)
-    title = serializers.CharField(max_length=50)
-    image = serializers.CharField(max_length=250)
-    content = serializers.CharField()
-    author_id = serializers.IntegerField()
-    tags_list = serializers.ListField(
-        child = serializers.CharField(max_length=25),
-        required = False,
-    )
-
-    def create(self, validated_data):
-        pass
-
-    def update(self, instance, validated_data):
-        pass
+class NewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = ['title', 'image', 'content', 'author', 'tags']
