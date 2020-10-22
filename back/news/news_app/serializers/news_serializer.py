58469d5d5a2d9582e724b497396596eb9a60d96c
Pavel Kotlyarov <p.kotlyarov@dunice.net>
2020-10-22 11:59:27 +0300

    tag_serializer and get_tagsList instead of tags id

--- back/news/news_app/serializers/news_serializer.py
+++ back/news/news_app/serializers/news_serializer.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
-from ..models import News
+from django.db.models import Q
+
+from ..models import News, Tag
+from .tag_serializer import TagSerializer
 
 
 class NewsSerializer(serializers.ModelSerializer):
+
+    tagsList = serializers.SerializerMethodField()
+    def get_tagsList(self, obj):
+            query = Q(id=-1)
+            for tag in obj.tags.all():
+                query.add(Q(id=tag.id), Q.OR)
+            serialized_data = TagSerializer(Tag.objects.filter(query), many=True).data
+            tags_data = []
+            for item in serialized_data:
+                tags_data += [dict(item)['title']]
+            return tags_data
+
     class Meta:
         model = News
-        fields = ['title', 'image', 'content', 'author', 'tags']
+        fields = ['title', 'image', 'content', 'author', 'tagsList']
