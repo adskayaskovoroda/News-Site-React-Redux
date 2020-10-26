3e4a05f8a4f66a8e424329dcff2cec5686e5cf06
Pavel Kotlyarov <p.kotlyarov@dunice.net>
2020-10-26 11:57:19 +0300

    removed dev_data and getPosts from App.jsx

--- front/news/src/App.jsx
+++ front/news/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux'
-import { getPosts, requestPosts } from './store/actions/actionCreators';
+import { requestPosts } from './store/actions/actionCreators';
 import MainPage from './pages/mainPage';
 
 function App({ requestPosts }) {
