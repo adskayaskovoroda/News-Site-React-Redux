a9ae88c52abe3c7c950650b806a2ea295f71f502
Pavel Kotlyarov <p.kotlyarov@dunice.net>
2020-10-27 16:28:58 +0300

    author to author_data

--- back/news/news_app/serializers/news_serializer.py
+++ back/news/news_app/serializers/news_serializer.py
@@ -2,20 +2,29 @@ from rest_framework import serializers
 from django.db.models import Q
 from django.contrib.postgres.aggregates.general import ArrayAgg
 
-from ..models import News, Tag
+from ..models import News, Tag, User
 from .tag_serializer import TagSerializer
 
 
 class NewsSerializer(serializers.ModelSerializer):
 
-    tagsList = serializers.SerializerMethodField()
-    def get_tagsList(self, obj):
-            query = Q(id=-1)
-            for tag in obj.tags.all():
-                query.add(Q(id=tag.id), Q.OR)
-            tags_data = Tag.objects.filter(query).aggregate(arr=ArrayAgg('title'))
-            return tags_data['arr']
+    tags_list = serializers.SerializerMethodField()
+    def get_tags_list(self, obj):
+        query = Q(id=-1)
+        for tag in obj.tags.all():
+            query.add(Q(id=tag.id), Q.OR)
+        tags_data = Tag.objects.filter(query).aggregate(arr=ArrayAgg('title'))
+        return tags_data['arr']
+
+    author_data = serializers.SerializerMethodField()
+    def get_author_data(self, obj):
+        author = User.objects.get(pk=obj.author.id)
+        author_data = {
+            'id': author.id,
+            'username': author.name,
+        }
+        return author_data
 
     class Meta:
         model = News
-        fields = ['id','title', 'image', 'content', 'author', 'tagsList']
+        fields = ['id','title', 'image', 'content', 'author_data', 'tags_list']
