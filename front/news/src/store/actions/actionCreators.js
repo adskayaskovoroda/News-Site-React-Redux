52d17eb94e675125e7fbea1eee3b150a51387ee7
Pavel Kotlyarov <p.kotlyarov@dunice.net>
2020-10-27 16:37:02 +0300

    get with search saga

--- front/news/src/store/actions/actionCreators.js
+++ front/news/src/store/actions/actionCreators.js
@@ -1,22 +1,10 @@
-import DEV_DATA from '../dev_data';
 import {
-  GET_POSTS,
   REQUEST_POSTS,
 } from './types';
 
-export function getPosts(id) {
-  if (id) return {
-    type: GET_POSTS,
-    payload: [DEV_DATA.find(el => el.id === id)],
-  }
-  return {
-    type: GET_POSTS,
-    payload: DEV_DATA,
-  }
-}
-
-export function requestPosts() {
+export function requestPosts(url = '') {
   return {
     type: REQUEST_POSTS,
+    url,
   }
 }
