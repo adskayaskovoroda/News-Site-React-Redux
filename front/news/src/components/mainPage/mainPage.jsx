48352b5a8b2a3999c76517bdf35149a3930c6f12
Pavel Kotlyarov <p.kotlyarov@dunice.net>
2020-10-28 15:12:03 +0300

    pagination

--- front/news/src/components/mainPage/mainPage.jsx
+++ front/news/src/components/mainPage/mainPage.jsx
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import Pagination from '@material-ui/lab/Pagination';
 import TopPanel from '../topPanel/topPanel';
 import NewsList from '../news/newsList';
 import NewsCard from '../news/newsCard';
 
+import './mainPage.css';
+
+const ITEMS_PER_PAGE = 1;
+
 function MainPage() {
-  const posts = useSelector(state => state.posts)
+  const posts = useSelector(state => state.posts);
+  const [curPage, setPage] = useState(1);
+  const changePage = (e, newPage) => {
+    setPage(newPage)
+  };
+
+  const start = () => (curPage - 1) * ITEMS_PER_PAGE;     // start for slice
+  const end = () => start() + ITEMS_PER_PAGE;             // end for slice
+  const pagesAmount = () => posts.length / ITEMS_PER_PAGE
 
   return (
     <>
       <TopPanel />
+      <Pagination
+        className="pagination"
+        count={pagesAmount()}
+        variant="outlined"
+        color="primary"
+        size="large"
+        page={curPage}
+        onChange={changePage}
+      />
       <NewsList>
-        {posts.map(el => <NewsCard data={el} key={el.id} />)}
+        {
+          posts.length
+            ? posts.slice(start(),end()).map(el => <NewsCard data={el} key={el.id} />)
+            : <div className="no-posts">There Are No Posts Here!</div>
+        }
       </NewsList>
+      <Pagination
+        className="pagination"
+        count={pagesAmount()}
+        variant="outlined"
+        color="primary"
+        size="large"
+        page={curPage}
+        onChange={changePage}
+      />
     </>
   );
 }
