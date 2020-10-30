a458286285162960935b32598a377d1996f83322
Pavel Kotlyarov <p.kotlyarov@dunice.net>
2020-10-30 17:31:41 +0300

    loader

--- front/news/src/store/sagas.js
+++ front/news/src/store/sagas.js
@@ -3,6 +3,9 @@ import {
   GET_POSTS,
   REQUEST_POSTS,
 } from './actions/types';
+import {
+  changeLoader
+} from './actions/actionCreators';
 import axios from 'axios'; 
 
 const SERVER_URL = 'http://127.0.0.1:8000';
@@ -12,11 +15,13 @@ export function* sagaWatcher() {
 }
 
 function* sagaWorker(action) {
+  yield put(changeLoader(true));
   const payload = yield call(fetchPosts, action.url);
   yield put({
     type: GET_POSTS,
     payload,
   });
+  yield put(changeLoader(false));
 }
 
 async function fetchPosts(url) {
