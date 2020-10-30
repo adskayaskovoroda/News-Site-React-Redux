50c11102979cd6381670fd5bb389d70c32bf068a
Pavel Kotlyarov <p.kotlyarov@dunice.net>
2020-10-30 17:32:03 +0300

    show loader

--- front/news/src/components/mainPage/mainPage.jsx
+++ front/news/src/components/mainPage/mainPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Pagination from '@material-ui/lab/Pagination';
+import { CircularProgress } from '@material-ui/core';
 import TopPanel from '../topPanel/topPanel';
 import NewsList from '../news/newsList';
 import NewsCard from '../news/newsCard';
@@ -12,6 +13,7 @@ const PAGE_CAP = 1;
 
 function MainPage() {
   const posts = useSelector(state => state.posts);
+  const loaderStatus = useSelector(state => state.showLoader);
   const [pageItems, setPage, binder] = usePagination(posts, PAGE_CAP);
   useEffect(() => setPage(1), [posts]);
 
@@ -27,9 +29,11 @@ function MainPage() {
       />
       <NewsList>
         {
-          pageItems.length
-            ? pageItems.map(el => <NewsCard data={el} key={el.id} />)
-            : <div className="no-posts">There Are No Posts Here!</div>
+          loaderStatus
+            ? <CircularProgress />
+            : pageItems.length
+              ? pageItems.map(el => <NewsCard data={el} key={el.id} />)
+              : <div className="no-posts">There Are No Posts Here!</div>
         }
       </NewsList>
       <Pagination
