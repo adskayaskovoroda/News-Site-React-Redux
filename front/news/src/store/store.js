a3729f654ff9f69f58c384a09d71da3d4fd57c76
Pavel Kotlyarov <p.kotlyarov@dunice.net>
2020-10-30 17:32:11 +0300

    minor fixes

--- front/news/src/store/store.js
+++ front/news/src/store/store.js
@@ -3,10 +3,15 @@ import createSagaMiddleware from 'redux-saga'
 import rootReducer from './reducers/rootReducer';
 import { sagaWatcher } from './sagas';
 
+const initialState = {
+  posts: [],
+  showLoader: false,
+}
+
 const saga = createSagaMiddleware();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(
+const store = createStore(rootReducer, initialState, composeEnhancers(
   applyMiddleware(saga)
 ));
 
