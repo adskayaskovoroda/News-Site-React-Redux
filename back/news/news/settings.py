bbd2c859e64ecb39087815d7c3ca4667f4f95c8a
Pavel Kotlyarov <p.kotlyarov@dunice.net>
2020-11-06 17:41:23 +0300

    added "django-rest-framework-social-oauth2"

--- back/news/news/settings.py
+++ back/news/news/settings.py
@@ -42,8 +42,10 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
 
-    # django-google-auth
-    'google_auth',
+    # OAuth2
+    'oauth2_provider',
+    'social_django',
+    'rest_framework_social_oauth2',
 ]
 
 # ====================================================================
@@ -53,20 +55,35 @@ MEDIA_URL = '/media/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'google_auth.authentication.GoogleAuthAuthentication',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+        'rest_framework_social_oauth2.authentication.SocialAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
 
 AUTHENTICATION_BACKENDS = [
-    'google_auth.authentication.GoogleAuthBackend',
+    'social_core.backends.google.GoogleOAuth2',
+    'rest_framework_social_oauth2.backends.DjangoOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-GOOGLE_AUTH_CLIENT_ID = '399978654308-3e8ua5aad8pbsmrgm6ufav37omiubn3f.apps.googleusercontent.com'
-GOOGLE_AUTH_CLIENT_SECRET = 'kE3ewEIaHE-BlmbRYRzm_Yu7'
-GOOGLE_AUTH_AUTHORIZED_DOMAINS = ['gmail.com','dunice.net']
-GOOGLE_AUTH_REDIRECT_URL = 'http://127.0.0.1:8000/posts/'
+# google
+# SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '399978654308-3e8ua5aad8pbsmrgm6ufav37omiubn3f.apps.googleusercontent.com'
+# SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'kE3ewEIaHE-BlmbRYRzm_Yu7'
+
+# django admin
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = 'rgC2vQAz2cbgAgZwkcj6OCDhtwGnJjEqX24uqESU'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'Hb0T9ZOxp392gHVwNCNptOaeqjZDbtRysKd6hzaBZFH0uQdrebylQ4PglaFJ6scdHZhi5br2jyzqsa9TfAJHuN8JUaUGgwcFpJrpWdvFTtWd8NtaIrFyuKb7DD5p1QDr'
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile',
+]
+
+# Google?
+# DRFSO2_PROPRIETARY_BACKEND_NAME = ''
+
+# DRFSO2_URL_NAMESPACE = ''
 
 AUTH_USER_MODEL = 'news_app.User'
 
@@ -77,6 +94,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -97,6 +115,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
--- back/news/news_app/urls.py
+++ back/news/news_app/urls.py
@@ -11,7 +11,7 @@ router = SimpleRouter()
 router.register('posts', PostViewSet)
 
 urlpatterns = [
-    url('auth/google/', include('google_auth.urls')),
+    url('auth/google/', include('rest_framework_social_oauth2.urls')),
     url('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     url('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     url('', include(router.urls)),
